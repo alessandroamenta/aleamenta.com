90b99e7a6de3d36abd9d02cfe23f1cbde53a816b
Alessandro Amenta <100393279+alessandroamenta@users.noreply.github.com>
2024-06-07 17:32:11 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,377 @@
+import { Suspense } from 'react';
+import { unstable_noStore as noStore } from 'next/cache';
+import Link from 'next/link';
+import Image from 'next/image';
+//import smashing from 'public/images/home/smashing.jpg';
+//import reactathon from 'public/images/home/reactathon.jpg';
+//import ship from 'public/images/home/ship.jpg';
+//import filming from 'public/images/home/filming.jpg';
+//import meetups from 'public/images/home/meetups.jpg';
+//import vercel from 'public/images/home/vercel.jpg';
+import building from 'public/images/home/building.jpeg';
+import reading from 'public/images/home/reading.jpeg';
+import github from 'public/images/home/github.jpeg';
+import running from 'public/images/home/running.jpeg';
+import hustling from 'public/images/home/hustling.jpeg';
+import cooking from 'public/images/home/cooking.jpeg';
+import avatar from 'app/avatar.jpg';
+import ViewCounter from 'app/blog/view-counter';
+import { PreloadResources } from 'app/preload';
+import {
+  getLeeYouTubeSubs,
+  getVercelYouTubeSubs,
+  getViewsCount,
+} from 'app/db/queries';
+
+function Badge(props) {
+  return (
+    <a
+      {...props}
+      target="_blank"
+      className="inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100"
+    />
+  );
+}
+
+function ArrowIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
+function ChannelLink({ img, link, name }) {
+  return (
+    <div className="group flex w-full">
+      <a
+        href={link}
+        target="_blank"
+        className="flex w-full items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800"
+      >
+        <div className="flex items-center space-x-3">
+          <div className="relative h-16">
+            <Image
+              alt={name}
+              src={img}
+              height={64}
+              width={64}
+              sizes="33vw"
+              className="h-16 w-16 rounded-full border border-neutral-200 dark:border-neutral-700"
+              priority
+            />
+            <div className="relative -right-10 -top-6 inline-flex h-6 w-6 items-center rounded-full border border-neutral-200 bg-white p-1 dark:border-neutral-700">
+              <svg width="15" height="11" role="img" aria-label="YouTube logo">
+                <use href="/sprite.svg#youtube" />
+              </svg>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <p className="font-medium text-neutral-900 dark:text-neutral-100">
+              {name}
+            </p>
+            <Suspense fallback={<p className="h-6" />}>
+              <Subs name={name} />
+            </Suspense>
+          </div>
+        </div>
+        <div className="transform text-neutral-700 transition-transform duration-300 group-hover:-rotate-12 dark:text-neutral-300">
+          <ArrowIcon />
+        </div>
+      </a>
+    </div>
+  );
+}
+
+async function Subs({ name }: { name: string }) {
+  noStore();
+  let subscribers;
+  if (name === '@ale_amenta') {
+    subscribers = await getLeeYouTubeSubs();
+  } else {
+    subscribers = await getVercelYouTubeSubs();
+  }
+
+  return (
+    <p className="text-neutral-600 dark:text-neutral-400">
+      {subscribers} subscribers
+    </p>
+  );
+}
+
+function BlogLink({ slug, name }) {
+  return (
+    <div className="group">
+      <a
+        href={`/blog/${slug}`}
+        className="flex w-full items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800"
+      >
+        <div className="flex flex-col">
+          <p className="font-medium text-neutral-900 dark:text-neutral-100">
+            {name}
+          </p>
+          <Suspense fallback={<p className="h-6" />}>
+            <Views slug={slug} />
+          </Suspense>
+        </div>
+        <div className="transform text-neutral-700 transition-transform duration-300 group-hover:-rotate-12 dark:text-neutral-300">
+          <ArrowIcon />
+        </div>
+      </a>
+    </div>
+  );
+}
+
+async function Views({ slug }: { slug: string }) {
+  let views = await getViewsCount();
+  return <ViewCounter allViews={views} slug={slug} />;
+}
+
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my portfolio</h1>
+      <PreloadResources />
+      <h1 className="mb-8 text-2xl font-medium tracking-tighter">
+        hey, I'm Ale 👋
+      </h1>
       <p className="prose prose-neutral dark:prose-invert">
-        This is your new portfolio.
+        {`I'm a self-taught full-stack engineer and indie hacker. I currently `}
+        <Link href="/work">work</Link>
+        {` as an AI Engineer at `}
+        <span className="not-prose">
+          <Badge href="https://www.nexler.nl/">
+            <svg
+              width="13"
+              height="11"
+              role="img"
+              aria-label="Vercel logo"
+              className="mr-1 inline-flex"
+            >
+              <use href="/sprite.svg#vercel" />
+            </svg>
+            Nexler
+          </Badge>
+        </span>
+        {`, a dutch startup where I help automate all boring things with AI.`}
+
+        {`I also run a dev agency where I partner with founders and soloprenours to build proof of concepts and MVPs. On nights and weekeds, I'm building my own indie projects. I enjoy running, travelling and reading books. Here are some images of momenets doing my favorite things.`}
+        <Badge href="https://react.dev">
+          <svg
+            width="14"
+            height="14"
+            role="img"
+            aria-label="React logo"
+            className="!mr-1"
+          >
+            <use href="/sprite.svg#react" />
+          </svg>
+          React
+        </Badge>
+        .
       </p>
+      <div className="grid grid-cols-2 grid-rows-4 sm:grid-rows-3 sm:grid-cols-3 gap-4 my-8">
+        <div className="relative h-40">
+          <Image
+            alt="Me speaking on stage at React Summit about the future of Next.js"
+            src={building}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="relative sm:row-span-2 row-span-1">
+          <Image
+            alt="Me standing on stage at Reactathon delivering the keynote"
+            src={reading}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover object-top sm:object-center"
+          />
+        </div>
+        <div className="relative">
+          <Image
+            alt="Me and Guillermo Rauch on stage for Vercel Ship, answering questions from the Next.js community"
+            src={github}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="relative row-span-2">
+          <Image
+            alt="Me, Lydia, and Delba filming the Next.js Conf keynote"
+            src={running}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover sm:object-center"
+          />
+        </div>
+        <div className="relative row-span-2">
+          <Image
+            alt="My badge on top of a pile of badges from a Vercel meetup we held"
+            src={hustling}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="relative h-40">
+          <Image
+            alt="Me standing on stage at SmashingConf giving a talk about my optimism for the web"
+            src={cooking}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+      </div>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          Some tech I love building with: 
+          <Badge href="https://nextjs.org">
+            <svg
+              width="14"
+              height="14"
+              role="img"
+              aria-label="Next.js logo"
+              className="!mr-1"
+            >
+              <use href="/sprite.svg#nextjs" />
+            </svg>
+            Next.js
+          </Badge>
+          <Badge href="https://react.dev">
+            <svg
+              width="14"
+              height="14"
+              role="img"
+              aria-label="React logo"
+              className="!mr-1"
+            >
+              <use href="/sprite.svg#react" />
+            </svg>
+            React
+          </Badge>
+        </p>
+      </div>
+      <div className="my-8 flex w-full flex-col space-x-0 space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+        <ChannelLink
+          img={avatar}
+          name="@leerob"
+          link="https://www.youtube.com/@leerob"
+        />
+        <ChannelLink
+          img={running}
+          name="@vercel"
+          link="https://www.youtube.com/@vercelhq"
+        />
+      </div>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          Over the past decade, I've written content on my blog and newsletter.
+          I try to keep things simple. You'll find writing about technologies
+          I'm interested in at the time, or how I'm learning and growing in my
+          career, sharing knowledge along the way.
+        </p>
+      </div>
+      <div className="my-8 flex w-full flex-col space-y-4">
+        <BlogLink
+          name="What Makes A Great Developer Experience?"
+          slug="developer-experience"
+        />
+        <BlogLink name="What is Developer Relations?" slug="devrel" />
+        <BlogLink name="The Story of Heroku" slug="heroku" />
+      </div>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          I invest small angel checks into early stage startups building tools
+          for developers.
+        </p>
+      </div>
+      <div className="my-8 flex h-14 w-full flex-row space-x-2 overflow-x-auto">
+        <div className="flex items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800">
+          <a href="https://linear.app">
+            <svg width="78" height="20" role="img" aria-label="Linear logo">
+              <use href="/sprite.svg#linear" />
+            </svg>
+          </a>
+        </div>
+        <div className="flex items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800">
+          <a href="https://supabase.com">
+            <svg width="100" height="19" role="img" aria-label="Supabase logo">
+              <use href="/sprite.svg#supabase" />
+            </svg>
+          </a>
+        </div>
+        <div className="flex items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800">
+          <a href="https://www.makeswift.com/blog/makeswift-is-joining-bigcommerce">
+            <svg width="96" height="19" role="img" aria-label="Makeswift logo">
+              <use href="/sprite.svg#makeswift" />
+            </svg>
+          </a>
+        </div>
+        <div className="flex items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800">
+          <a href="https://resend.com">
+            <svg width="70" height="17" role="img" aria-label="Resend logo">
+              <use href="/sprite.svg#resend" />
+            </svg>
+          </a>
+        </div>
+        <div className="flex items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800">
+          <a href="https://bun.sh">
+            <svg width="35" height="27" role="img" aria-label="Bun logo">
+              <use href="/sprite.svg#bun" />
+            </svg>
+          </a>
+        </div>
+      </div>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          I've worked with and advised companies on{' '}
+          <Link href="/blog/developer-marketing">developer marketing</Link>,{' '}
+          <Link href="/blog/devrel">developer relations</Link>, building
+          open-source communities, product-led growth, and more.
+        </p>
+      </div>
+      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://twitter.com/leeerob"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">follow me</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://leerob.substack.com"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">get email updates</p>
+          </a>
+        </li>
+      </ul>
     </section>
   );
 }
