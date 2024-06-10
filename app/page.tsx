3cfdbaf08f56cf899dd7b4d009187c86da614f37
Alessandro Amenta <100393279+alessandroamenta@users.noreply.github.com>
2024-06-10 12:49:29 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,6 @@ import avatar1 from 'app/avatar1.png';
 import { PreloadResources } from 'app/preload';
 import { getViewsCount } from 'app/db/queries';
 
-
 function Badge(props) {
   return (
     <a
